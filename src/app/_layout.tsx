7f3da9eb49ee4ddaaebd201ccee97a5ae1bce1b8
Victor Canissaris Furlan <victorcfurlan@gmail.com>
2026-09-21 22:45:04 -0300

    fix(layout): remove navigationBar bugada, e arruma algumas cores

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,21 +1,35 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import { Stack } from 'expo-router';
+import { Stack, usePathname } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import * as SystemUI from 'expo-system-ui'
 import { useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { NavigationBar } from 'expo-navigation-bar';
 import { TemaProvider, useTema } from '@/contexts/temaContexto';
+import { CoresClaro, CoresEscuro } from '@/constants/cores';
 
 SplashScreen.preventAutoHideAsync();
 
 function AppLayout() {
   const { isDark } = useTema();
+  const pathname = usePathname();
+
+  const isTabs =
+    pathname !== "/" &&
+    pathname !== "/criar_conta" &&
+    pathname !== "esqueci_minha_senha";
 
   useEffect(() => {
     SplashScreen.hideAsync();
-  }, []);
+    {/*essas duas linhas aq trocam a cor do fundo do sistema de acordo com o tema selecionado */}
+    const cor = isTabs ? (isDark ? CoresEscuro.bg : CoresClaro.bg) : (isDark ? CoresEscuro.card : CoresClaro.card)
+    SystemUI.setBackgroundColorAsync(cor)
+  }, [isDark, isTabs]);
 
   return (
     <ThemeProvider value={isDark ? DarkTheme : DefaultTheme}>
+      {/*o navigationbar server pra sumir e aparecer com a barrinha que tem o botão de voltar, sair, etc, do celular*/}
+      <NavigationBar hidden={true} />
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
         <Stack.Screen name="criar_conta" />
